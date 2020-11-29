fa07b39064ecaeeb2920e5bd0f2456927f70bc47
Maven <milosz.fela@gmail.com>
2020-11-29 10:33:16 +0100

    Imported RouterModule for routes in app.module.ts
    Created basic folder structure in src/app
    
    Added bootstrap support in src/index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { LoggingComponent } from './component/logging/logging.component';
+
+const routes: Routes = [{
+  // path: '...',
+  // component:
+}, {
+  // path: '...',
+  // component:
+}, {
+  // path: '...',
+  // component:
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoggingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
