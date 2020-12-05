ad562d916c355ae4c56deabdf766632ada370ced
Jakub Nowak <nowakjakub095@gmail.com>
2020-12-05 10:11:40 +0100

    fixed carousel and added my account disable enable feature

--- src/app/component/carousel/carousel.component.ts
+++ src/app/component/carousel/carousel.component.ts
@@ -7,29 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  activeSlide = 0;
-
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  prevSlide(): void {
-    if (this.activeSlide === 0){
-      this.activeSlide = 2;
-    }
-    else {
-      this.activeSlide --;
-    }
-  }
-
-  nextSlide(): void {
-    if (this.activeSlide === 2) {
-      this.activeSlide = 0;
-    }
-    else {
-      this.activeSlide ++;
-    }
   }
 
 }
